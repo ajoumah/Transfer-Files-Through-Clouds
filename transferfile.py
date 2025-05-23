ee45d1b15c50d7e7656fc20dd888f34d66b5dcf0
ajoumah <ahmadjoumah5@gmail.com>
2025-05-23 23:18:42 +0300

    Update transferfile.py

--- transferfile.py
+++ transferfile.py
@@ -3,25 +3,25 @@
 """
 
 import requests
-file_url = "https://drive.google.com/file/d/1iQV5kKF_KGZL9ALx9MMXk_Lg7PklBLCE/view"
+file_url = "Path/KF_KGZL9ALx9MMXk_Lg7PklBLCE/view"
 
 r = requests.get(file_url, stream = True)
 
-with open("/content/drive/MyDrive/multiLabel1/aclImdb.zip", "wb") as file:
+with open("Path/aclImdb.zip", "wb") as file:
     for block in r.iter_content(chunk_size = 1024):
          if block:
              file.write(block)
 
-#!unzip "/content/drive/MyDrive/IMBD1/aclImdb.zip"
-!unzip  "/content/drive/MyDrive/Multi_Label_dataset.zip"  -d "/content/drive/MyDrive/UnMulti/"
+#!unzip "Path/aclImdb.zip"
+!unzip  "Path/Multi_Label_dataset.zip"  -d "Path/UnMulti/"
 
 from shutil import copyfile
-copyfile("/content/drive/MyDrive/coco_Dataset", "/content/drive/MyDrive/Wheelchair obstacle detection /Ahmad/MS_COCO")
+copyfile("Path/coco_Dataset", "Path/MS_COCO")
 
 from distutils.dir_util import copy_tree
 
 # copy subdirectory example
-fromDirectory = "/content/drive/MyDrive/Video2images"
-toDirectory = "/content/drive/MyDrive/Wheelchair obstacle detection /Ahmad/Video_Images"
+fromDirectory = "Path/Video2images"
+toDirectory = "Path/Video_Images"
 
 copy_tree(fromDirectory, toDirectory)
